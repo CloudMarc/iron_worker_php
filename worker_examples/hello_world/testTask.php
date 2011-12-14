c6132aa1ddd2b7865daffd89325ba2a82611091f
thousandsofthem <thousandsofthem@gmail.com>
2011-12-15 00:51:44 +0300

    Added super-simple task. Getting all arguments, not just payload for 'hello_world'.

--- worker_examples/hello_world/testTask.php
+++ worker_examples/hello_world/testTask.php
@@ -1,17 +1,26 @@
 <?php
 
-function getPayload($argv){
+function getArgs(){
+    global $argv;
+    $args = array('payload' => array(), 'task_id' => null, 'dir' => null);
     foreach($argv as $k => $v){
+        if ($v == '-id' && !empty($argv[$k+1])){
+            $args['task_id'] = $argv[$k+1];
+        }
+        if ($v == '-d' && !empty($argv[$k+1])){
+            $args['dir'] = $argv[$k+1];
+        }
         if ($v == '-payload' && !empty($argv[$k+1]) && file_exists($argv[$k+1])){
-            return json_decode(file_get_contents($argv[$k+1]));
+            $args['payload'] = json_decode(file_get_contents($argv[$k+1]));
         }
     }
-    return array();
+    return $args;
 }
+
+$args = getArgs();
+
 
 echo "Hello PHP World!!!\n";
 echo "at " . date('r') . "\n";
 
-print_r($argv);
-
-print_r(getPayload($argv));
+print_r($args);
